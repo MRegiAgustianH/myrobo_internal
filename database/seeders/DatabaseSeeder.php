f14d7ba4fe2204b6484c9df8430cb1b5671a9ac7
MRegiAgustian <muhmmad.regi@gmail.com>
2026-09-24 13:42:15 +0700

    chore: perbarui .gitignore dan untrack file scratch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             KompetensiSeeder::class,
             IndikatorKompetensiSeeder::class,
             PermissionSeeder::class,
+            ModuleSettingSeeder::class,
         ]);
     }
 }
